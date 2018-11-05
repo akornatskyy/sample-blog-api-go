6f7a0ed4dfdaed5e4e0225cf7617c89b7468d4db
Andriy Kornatskyy <andriy.kornatskyy@live.com>
2018-11-05 20:17:11 +0200

    Introduced signin handler.

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"log"
 	"net/http"
+
+	m "github.com/akornatskyy/sample-blog-api-go/membership/infrastructure"
 )
 
 const (
@@ -10,6 +12,8 @@ const (
 )
 
 func main() {
+	http.HandleFunc("/signin", m.SignInHandler)
+
 	log.Printf("listening on %s", addr)
 	log.Fatal(http.ListenAndServe(addr, nil))
 }
---
+++ membership/infrastructure/signin.go
@@ -0,0 +1,17 @@
+package infrastructure
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+func SignInHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(&struct{}{})
+}
