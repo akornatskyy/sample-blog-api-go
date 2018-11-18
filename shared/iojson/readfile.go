4a584432c788e5a3a1dda06739e3ecb86ad768b4
Andriy Kornatskyy <andriy.kornatskyy@live.com>
2018-11-18 20:57:38 +0200

    Introduced iojson package to read file.

---
+++ shared/iojson/readfile.go
@@ -0,0 +1,19 @@
+package iojson
+
+import (
+	"encoding/json"
+	"io/ioutil"
+)
+
+// ReadFile reads the file named by filename as JSON-encoded
+// value and stores it in the value pointed to by v.
+func ReadFile(filename string, v interface{}) error {
+	value, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return err
+	}
+	if err := json.Unmarshal(value, v); err != nil {
+		return err
+	}
+	return nil
+}
