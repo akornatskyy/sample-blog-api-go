dcd5e0f73c7ef1927166876bad4af4484cabd1bb
Andriy Kornatskyy <andriy.kornatskyy@live.com>
2018-11-19 20:58:34 +0200

    Added find auth info implementation to mock user repository.

--- membership/infrastructure/mock/user.go
+++ membership/infrastructure/mock/user.go
@@ -2,17 +2,53 @@ package mock
 
 import (
 	"errors"
+	"log"
 
 	"github.com/akornatskyy/sample-blog-api-go/membership/domain/user"
+	"github.com/akornatskyy/sample-blog-api-go/shared/iojson"
 )
 
 type userRepository struct {
 }
+
+type userInfo struct {
+	ID       int    `json:"id"`
+	Username string `json:"username"`
+	Password string `json:"password"`
+	IsLocked bool   `json:"is_locked"`
+}
+
+var users = loadUsers("user-samples.json")
 
 func NewUserRepository() user.Repository {
 	return userRepository{}
 }
 
 func (r userRepository) FindAuthInfo(username string) (*user.AuthInfo, error) {
-	return nil, errors.New("not found")
+	u, ok := users[username]
+	if !ok {
+		return nil, errors.New("not found")
+	}
+	m := user.AuthInfo{
+		ID:       u.ID,
+		IsLocked: u.IsLocked,
+		Password: u.Password,
+	}
+	return &m, nil
+}
+
+func loadUsers(filename string) map[string]userInfo {
+	var r map[string][]userInfo
+	if err := iojson.ReadFile(filename, &r); err != nil {
+		panic(err)
+	}
+
+	users := map[string]userInfo{}
+	for _, u := range r["users"] {
+		users[u.Username] = u
+	}
+
+	log.Printf("loaded %d users", len(users))
+
+	return users
 }
