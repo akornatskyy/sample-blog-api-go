87e8043b3ed29cb3048c31c882223a6dc7b04f89
Andriy Kornatskyy <andriy.kornatskyy@live.com>
2019-03-18 20:33:17 +0200

    Return status code 204 (no content) on sign out.

--- membership/infrastructure/signout.go
+++ membership/infrastructure/signout.go
@@ -9,5 +9,6 @@ import (
 func SignOutHandler(t httptoken.Token) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		t.Invalidate(w)
+		w.WriteHeader(http.StatusNoContent)
 	}
 }
