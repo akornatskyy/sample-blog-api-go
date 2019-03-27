431663d7c8750fde14a3587fb4416a1e745dd369
Andriy Kornatskyy <andriy.kornatskyy@live.com>
2019-03-27 20:17:05 +0200

    Exposed get user by id in mock repository.

--- membership/infrastructure/mock/user.go
+++ membership/infrastructure/mock/user.go
@@ -28,10 +28,10 @@ var (
 )
 
 func NewUserRepository() user.Repository {
-	return userRepository{}
+	return &userRepository{}
 }
 
-func (r userRepository) FindAuthInfo(username string) (*user.AuthInfo, error) {
+func (*userRepository) FindAuthInfo(username string) (*user.AuthInfo, error) {
 	u, ok := users[username]
 	if !ok {
 		return nil, errNotFound
@@ -44,7 +44,7 @@ func (r userRepository) FindAuthInfo(username string) (*user.AuthInfo, error) {
 	return &m, nil
 }
 
-func (r userRepository) FindUserByID(id string) (*user.User, error) {
+func FindUserByID(id string) (*user.User, error) {
 	for _, u := range users {
 		if u.ID == id {
 			m := user.User{
@@ -59,17 +59,21 @@ func (r userRepository) FindUserByID(id string) (*user.User, error) {
 	return nil, errNotFound
 }
 
-func (r userRepository) HasAccount(username string) (bool, error) {
+func (*userRepository) FindUserByID(id string) (*user.User, error) {
+	return FindUserByID(id)
+}
+
+func (*userRepository) HasAccount(username string) (bool, error) {
 	_, ok := users[username]
 	return ok, nil
 }
 
-func (r userRepository) CreateAccount(reg *user.Registration) (bool, error) {
+func (*userRepository) CreateAccount(reg *user.Registration) (bool, error) {
 	id, err := uuid.NewRandom()
 	if err != nil {
 		return false, err
 	}
-	users[reg.Username] = userInfo{
+	users[reg.Username] = &userInfo{
 		ID:           id.String(),
 		Username:     reg.Username,
 		PasswordHash: string(reg.PasswordHash),
@@ -78,13 +82,13 @@ func (r userRepository) CreateAccount(reg *user.Registration) (bool, error) {
 	return true, nil
 }
 
-func loadUsers(filename string) map[string]userInfo {
-	var r map[string][]userInfo
+func loadUsers(filename string) map[string]*userInfo {
+	var r map[string][]*userInfo
 	if err := iojson.ReadFile(filename, &r); err != nil {
 		panic(err)
 	}
 
-	users := map[string]userInfo{}
+	users := map[string]*userInfo{}
 	for _, u := range r["users"] {
 		users[u.Username] = u
 	}
