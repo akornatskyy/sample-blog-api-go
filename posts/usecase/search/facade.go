ef610a59a2626aad07b25c18d578248471646032
Andriy Kornatskyy <andriy.kornatskyy@live.com>
2019-03-30 20:24:28 +0200

    Updated posts search facade with paging.

--- posts/usecase/search/facade.go
+++ posts/usecase/search/facade.go
@@ -1,9 +1,31 @@
 package search
 
+import (
+	"github.com/akornatskyy/sample-blog-api-go/posts/domain"
+)
+
 func Process(req *Request) (*Response, error) {
 	if err := req.Validate(); err != nil {
 		return nil, err
 	}
-	resp := Response{}
+	posts, err := domain.PostRepository().SearchPosts(
+		req.Query, PageSize+1, req.Page*PageSize)
+	if err != nil {
+		return nil, err
+	}
+	paging := &Paging{}
+	if req.Page > 0 {
+		b := req.Page - 1
+		paging.Before = &b
+	}
+	if len(posts) > PageSize {
+		posts = posts[:PageSize]
+		a := req.Page + 1
+		paging.After = &a
+	}
+	resp := Response{
+		Paging: paging,
+		Items:  posts,
+	}
 	return &resp, nil
 }
--- posts/usecase/search/models.go
+++ posts/usecase/search/models.go
@@ -1,11 +1,22 @@
 package search
 
+import "github.com/akornatskyy/sample-blog-api-go/posts/domain/post"
+
+const PageSize = 2
+
 type (
 	Request struct {
 		Query string `binding:"q"`
 		Page  int    `binding:"page"`
 	}
 
+	Paging struct {
+		Before *int `json:"before,omitempty"`
+		After  *int `json:"after,omitempty"`
+	}
+
 	Response struct {
+		Paging *Paging      `json:"paging"`
+		Items  []*post.Post `json:"items"`
 	}
 )
