6a7cf803709fc0467df781c7b83ec42bc2a47e32
Andriy Kornatskyy <andriy.kornatskyy@live.com>
2019-04-27 20:24:58 +0300

    Updated sql user repository with DB calls.

--- membership/infrastructure/sql/user.go
+++ membership/infrastructure/sql/user.go
@@ -2,30 +2,60 @@ package sql
 
 import (
 	"database/sql"
+	"log"
 
 	"github.com/akornatskyy/sample-blog-api-go/membership/domain/user"
+	"github.com/akornatskyy/sample-blog-api-go/shared/sqlx"
 )
 
 type userRepository struct {
+	SelectAuthInfo   *sql.Stmt `SELECT id, password_hash, is_locked FROM users WHERE username = ?`
+	SelectUserByID   *sql.Stmt `SELECT username, first_name, last_name FROM users where id = ?`
+	ExistsByUsername *sql.Stmt `SELECT '' FROM users WHERE username = ?`
+	InsertAccount    *sql.Stmt `INSERT INTO users (id, username, password_hash, email) VALUES (?, ?, ?, ?)`
 }
 
 func NewUserRepository(db *sql.DB) user.Repository {
 	r := &userRepository{}
+	if err := sqlx.Prepare(db, r); err != nil {
+		log.Fatal(err)
+	}
 	return r
 }
 
 func (r *userRepository) FindAuthInfo(username string) (*user.AuthInfo, error) {
-	return nil, nil
+	m := &user.AuthInfo{}
+	return m, r.SelectAuthInfo.QueryRow(username).Scan(
+		&m.UserID, &m.PasswordHash, &m.IsLocked,
+	)
 }
 
 func (r *userRepository) FindUserByID(id string) (*user.User, error) {
-	return nil, nil
+	m := &user.User{}
+	return m, r.SelectUserByID.QueryRow(id).Scan(
+		&m.Username, &m.FirstName, &m.LastName,
+	)
 }
 
 func (r *userRepository) HasAccount(username string) (bool, error) {
-	return true, nil
+	var s string
+	err := r.ExistsByUsername.QueryRow(username).Scan(&s)
+	switch {
+	case err == sql.ErrNoRows:
+		return false, nil
+	case err != nil:
+		return true, err
+	default:
+		return true, nil
+	}
 }
 
 func (r *userRepository) CreateAccount(reg *user.Registration) (bool, error) {
-	return false, nil
+	_, err := r.InsertAccount.Exec(
+		reg.UserID, reg.Username, reg.PasswordHash, reg.Email,
+	)
+	if err != nil {
+		return false, err
+	}
+	return true, nil
 }
