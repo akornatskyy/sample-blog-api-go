56a74a2bac4af6f36b45d37a6d5279fd942e6a29
Andriy Kornatskyy <andriy.kornatskyy@live.com>
2019-06-18 20:31:27 +0300

    Introduced must prepare into sqlx package.

--- membership/infrastructure/sql/user.go
+++ membership/infrastructure/sql/user.go
@@ -2,44 +2,48 @@ package sql
 
 import (
 	"database/sql"
-	"log"
 
 	"github.com/akornatskyy/sample-blog-api-go/membership/domain/user"
 	"github.com/akornatskyy/sample-blog-api-go/shared/sqlx"
 )
 
 type userRepository struct {
-	SelectAuthInfo   *sql.Stmt `SELECT id, password_hash, is_locked FROM users WHERE username = ?`
-	SelectUserByID   *sql.Stmt `SELECT username, first_name, last_name FROM users where id = ?`
-	ExistsByUsername *sql.Stmt `SELECT '' FROM users WHERE username = ?`
-	InsertAccount    *sql.Stmt `INSERT INTO users (id, username, password_hash, email) VALUES (?, ?, ?, ?)`
+	selectAuthInfo   *sql.Stmt
+	selectUserByID   *sql.Stmt
+	existsByUsername *sql.Stmt
+	insertAccount    *sql.Stmt
 }
 
 func NewUserRepository(db *sql.DB) user.Repository {
-	r := &userRepository{}
-	if err := sqlx.Prepare(db, r); err != nil {
-		log.Fatal(err)
+	return &userRepository{
+		selectAuthInfo: sqlx.MustPrepare(db, `
+			SELECT id, password_hash, is_locked FROM users WHERE username = ?`),
+		selectUserByID: sqlx.MustPrepare(db, `
+			SELECT username, first_name, last_name FROM users where id = ?`),
+		existsByUsername: sqlx.MustPrepare(db, `
+			SELECT '' FROM users WHERE username = ?`),
+		insertAccount: sqlx.MustPrepare(db, `
+			INSERT INTO users (id, username, password_hash, email) VALUES (?, ?, ?, ?)`),
 	}
-	return r
 }
 
 func (r *userRepository) FindAuthInfo(username string) (*user.AuthInfo, error) {
 	m := &user.AuthInfo{}
-	return m, r.SelectAuthInfo.QueryRow(username).Scan(
+	return m, r.selectAuthInfo.QueryRow(username).Scan(
 		&m.UserID, &m.PasswordHash, &m.IsLocked,
 	)
 }
 
 func (r *userRepository) FindUserByID(id string) (*user.User, error) {
 	m := &user.User{}
-	return m, r.SelectUserByID.QueryRow(id).Scan(
+	return m, r.selectUserByID.QueryRow(id).Scan(
 		&m.Username, &m.FirstName, &m.LastName,
 	)
 }
 
 func (r *userRepository) HasAccount(username string) (bool, error) {
 	var s string
-	err := r.ExistsByUsername.QueryRow(username).Scan(&s)
+	err := r.existsByUsername.QueryRow(username).Scan(&s)
 	switch {
 	case err == sql.ErrNoRows:
 		return false, nil
@@ -51,7 +55,7 @@ func (r *userRepository) HasAccount(username string) (bool, error) {
 }
 
 func (r *userRepository) CreateAccount(reg *user.Registration) (bool, error) {
-	_, err := r.InsertAccount.Exec(
+	_, err := r.insertAccount.Exec(
 		reg.UserID, reg.Username, reg.PasswordHash, reg.Email,
 	)
 	if err != nil {
--- shared/sqlx/prepare.go
+++ shared/sqlx/prepare.go
@@ -2,38 +2,16 @@ package sqlx
 
 import (
 	"database/sql"
-	"errors"
-	"fmt"
-	"reflect"
+	"log"
 )
 
-func Prepare(db *sql.DB, r interface{}) error {
-	v := reflect.ValueOf(r)
-	if v.Kind() != reflect.Ptr || v.IsNil() {
-		return errors.New("expects not nil ptr")
+func MustPrepare(db *sql.DB, query string) *sql.Stmt {
+	stmt, err := db.Prepare(query)
+	if err != nil {
+		log.Fatalf(
+			"sql.DB: Prepare(query) %s: %s",
+			err.Error(),
+			query)
 	}
-	v = v.Elem()
-	for i := 0; i < v.NumField(); i++ {
-		f := v.Field(i)
-		if !f.CanSet() {
-			continue
-		}
-		switch f.Interface().(type) {
-		case *sql.Stmt:
-			query := string(v.Type().Field(i).Tag)
-			if query == "" {
-				continue
-			}
-			stmt, err := db.Prepare(query)
-			if err != nil {
-				return fmt.Errorf(
-					"sql.DB: Prepare(query) for %s.%s: %s",
-					v.Type().Name(),
-					v.Type().Field(i).Name,
-					err.Error())
-			}
-			f.Set(reflect.ValueOf(stmt))
-		}
-	}
-	return nil
+	return stmt
 }
