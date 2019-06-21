d774a02b736714e2716900c4ee293598289d2419
Andriy Kornatskyy <andriy.kornatskyy@live.com>
2019-06-21 20:22:47 +0300

    Added graceful shutdown of http server on interrupt and terminated signals.

--- main.go
+++ main.go
@@ -2,8 +2,12 @@
 package main
 
 import (
+	"context"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
 
 	"github.com/akornatskyy/sample-blog-api-go/membership"
 	"github.com/akornatskyy/sample-blog-api-go/posts"
@@ -16,12 +20,42 @@ const (
 )
 
 func main() {
+	log.Printf("starting...")
 	c := config.New()
 
 	membership.Setup(c)
 	posts.Setup(c)
 	public.Setup(c)
 
-	log.Printf("listening on %s", addr)
-	log.Fatal(http.ListenAndServe(addr, c.Router))
+	log.Printf("http listening on %s", addr)
+	server := &http.Server{Addr: addr, Handler: c.Router}
+	go func() {
+		if err := server.ListenAndServe(); err != nil {
+			switch err {
+			case http.ErrServerClosed:
+				log.Println("http stopped")
+			default:
+				log.Printf("http serve: %s", err)
+			}
+		}
+	}()
+
+	ch := make(chan os.Signal)
+	signal.Notify(ch, syscall.SIGINT, syscall.SIGTERM)
+	<-ch
+
+	log.Println("shutting down...")
+	if err := server.Shutdown(context.Background()); err != nil {
+		log.Printf("server shutdown: %s", err)
+	}
+	if err := server.Close(); err != nil {
+		log.Printf("server close: %s", err)
+	}
+	if c.DB != nil {
+		if err := c.DB.Close(); err != nil {
+			log.Printf("db close: %s", err)
+		}
+	}
+
+	log.Println("done")
 }
